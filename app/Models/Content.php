d655713315f59015d517f3331860c5fd13d17e99
Hafssa22 <hafssaassou04@gmail.com>
2025-04-26 22:56:21 +0100

    Add files via upload

---
+++ app/Models/Content.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Content extends Model
+{
+    protected $fillable = [
+        'title',
+        'category',
+        'media_type',
+        'media_path',
+        'poster_path',
+    ];
+}
