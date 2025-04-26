418112b94bfebca1f57f7e3c1fffb994de89b7b8
Nada Souna <nada.souna@esi.ac.ma>
2025-04-26 23:29:48 +0100

    Add files via upload

---
+++ app/Http/Controllers/KidsController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\Content;
+
+class KidsController extends Controller
+{
+    public function index()
+    {
+        return view('kids.index');
+    }
+
+    public function showCategory($category)
+    {
+        $contents = Content::where('category', $category)
+            ->orderBy('title')
+            ->get()
+            ->groupBy('title');
+        return view('kids.category', compact('contents', 'category'));
+    }
+    public function abc()
+        {
+            $contents = Content::where('category', 'ABC')->get();
+
+            $grouped = $contents->groupBy('title');
+
+            return view('kids.abc', compact('grouped'));
+        }
+}
